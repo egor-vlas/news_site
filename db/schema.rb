1e50fb33c1808bc326d567602a1057e2a853c688
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-01 12:03:52 +1000

    Admin functionality added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_012418) do
+ActiveRecord::Schema.define(version: 2021_07_01_015855) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_01_012418) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "comments", "articles"
