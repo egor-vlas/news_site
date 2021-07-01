82f037309700c044cf0f06f9c5e3c7d3cb42e625
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-01 12:35:11 +1000

    Add one-to-many association between users and artices from back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_015855) do
+ActiveRecord::Schema.define(version: 2021_07_01_021125) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_01_015855) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
