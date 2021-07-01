f4c929e7a22fa5c2bf9d3167d9c91f79672584cd
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-01 11:52:23 +1000

    Add comment routes and model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+	has_many :comments
 	validates :title, presence: true, length: {minimum: 3, maximum: 250}
 	validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_003806) do
+ActiveRecord::Schema.define(version: 2021_07_01_012418) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_07_01_003806) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
