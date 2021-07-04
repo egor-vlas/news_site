c537721fd91f0693ab939477017071aa220231df
Iegor <vlasenkoiegor2021@gmail.com>
2021-07-04 10:33:51 +1000

    sign up functionalityadded

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	root 'articles#index'
+	
 	resources :articles do 
 		resources :comments
 	end
+
+	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
 end
